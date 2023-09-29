e7eac61c24e8f2428d760a1f3c280beb58e64036
omarrr111 <110401870+omarrr111@users.noreply.github.com>
2023-09-29 16:13:42 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let img = document.querySelector(".img")
+let body = document.querySelector("body")
+function phones(phone){
+    img.src = phone;
+}
+function colors(color){
+    body.style.background = color
+}
